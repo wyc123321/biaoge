950c6a5dfd40ed14ee9f313b92d8e398f9425515
xinxin1102 <649177898@qq.com>
2017-02-20 13:08:46 +0800

    kl

--- insert.php
+++ insert.php
@@ -3,9 +3,8 @@ include "db.php";
 $id=$_GET["id"];
 $attr=$_GET["attr"];
 $value=$_GET["value"];
-$sql="update stu set $attr='$value' where id=".$id;
-$result=$db->query($sql);
+$db->query("update stu set $attr='$value' where id=".$id);
 if($db->affected_rows>0){
-    echo "修改成功";
+    echo "ec";
 }
 ?>
